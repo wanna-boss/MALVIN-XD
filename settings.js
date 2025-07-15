4d580daf5cdcb1d8726d8f05c2d69430246c4979
wanna-boss <teristech7@gmail.com>
2025-07-15 19:11:55 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ function convertToBool(text, trueValue = 'true') {
 
 module.exports = {
   // ===== BOT CORE SETTINGS =====
-  SESSION_ID: process.env.SESSION_ID || "", // Your bot's session ID (keep secure)
+  SESSION_ID: process.env.SESSION_ID || "malvin~FrFyBALQ#dfq9yc3yq_Qe9IJqca6r6osjZVqkZLHGjFHq2oGjoIc", // Your bot's session ID (keep secure)
   PREFIX: getConfig("PREFIX") || ".", // Command prefix (e.g., ., /, !, *)
   CHATBOT: getConfig("CHATBOT") || "on", // Chatbot mode: on/off
   BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "ᴍᴀʟᴠɪɴ-xᴅ", // Bot display name
